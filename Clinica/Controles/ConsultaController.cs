598e85f6e11673d280ac8795ee1edbe390d78147
Haline <luisguilherme.lemes@gmail.com>
2018-06-29 10:51:01 -0300

    Alterações de botoes wfp main

--- Clinica/Controles/ConsultaController.cs
+++ Clinica/Controles/ConsultaController.cs
@@ -1,9 +1,9 @@
-﻿using Modelos;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelos;
 
 namespace Controles
 {
@@ -58,3 +58,4 @@ namespace Controles
     }
 
 }
+
--- Clinica/Controles/ContaController.cs
+++ Clinica/Controles/ContaController.cs
@@ -1,9 +1,9 @@
-﻿using Modelos;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modelos;
 
 namespace Controles
 {
